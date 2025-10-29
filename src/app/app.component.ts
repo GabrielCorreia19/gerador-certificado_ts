114a8982866a7faa65c14a1c24111c18da46228c
Gabriel Correia <gabriel.com.gc10@gmail.com>
2025-10-29 14:37:31 -0300

    feat: Cria todas as telas da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './_components/navbar/navbar.component';
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
-import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
-import { ItemCertificadoComponent } from "./_components/item-certificado/item-certificado.component";
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { CertificadoComponent } from './pages/certificado/certificado.component';
+import { CertificadoFormComponent } from "./pages/certificado-form/certificado-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent],
+  imports: [NavbarComponent, BaseUiComponent, CertificadoComponent, CertificadoFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
